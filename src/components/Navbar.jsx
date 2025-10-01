25328f8f2635df32e23bd4512e7c529e511cbe64
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2025-10-02 01:02:50 +0500

    first commifst

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,92 @@
-import React, { useState } from 'react'
-import { motion } from 'motion/react'
-import assets from '../assets/assets'
-import ToggleBtn from './ToggleBtn'
+import React, { useState } from "react";
+import { motion } from "motion/react";
+import assets from "../assets/assets";
+import ToggleBtn from "./ToggleBtn";
 
-const Navbar = ({theme , setTheme}) => {
-  const [sideBar, setSidebar] = useState(false)
+const Navbar = ({ theme, setTheme }) => {
+  const [sideBar, setSidebar] = useState(false);
 
   return (
     <motion.div
-    initial={{opacity: 0, y: -50}}
-    animate={{opacity: 1, y: 0}}
-    transition={{duration: 1, ease: 'easeOut'}}
-    className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20
-    backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/700'>
-      <img src={theme === "dark" ? assets.logo_dark : assets.logo} className='w-32 sm:w-40 cursor-pointer' alt="" />
+      initial={{ opacity: 0, y: -50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: "easeOut" }}
+      className="flex justify-between items-center text-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20
+    backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/700"
+    >
+      <h1
+        className={`text-xl font-extrabold ${
+          theme === "dark" ? "text-white" : "text-black"
+        }`}
+      >
+        M Faizan Ali
+      </h1>
+      {/* <img src={theme === "dark" ? assets.logo_dark : assets.logo} className='w-32 sm:w-40 cursor-pointer' alt="" /> */}
 
-      <div className={`text-gray-700 dark:text-white sm:text-sm ${!sideBar ? 'max-sm:w-0 overflow-hidden' : 
-      'max-sm:w-60 max-sm:pl-10'} max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen 
+      <div
+        className={`text-gray-700 dark:text-white sm:text-sm ${
+          !sideBar ? "max-sm:w-0 overflow-hidden" : "max-sm:w-60 max-sm:pl-10"
+        } max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen 
       max-sm:h-full max-sm:gap-15 max-sm:text-xl max-sm:flex-col max-sm:bg-primary max-sm:text-white
-      max-sm:pt-20 flex sm:items-center gap-5 transition-all`}>
+      max-sm:pt-20 flex sm:items-center gap-5 transition-all`}
+      >
+        <img
+          src={assets.close_icon}
+          className="w-5 absolute right-4 top-4 sm:hidden cursor-pointer"
+          onClick={() => setSidebar(false)}
+          alt=""
+        />
 
-        <img src={assets.close_icon} className='w-5 absolute right-4 top-4 sm:hidden cursor-pointer'
-        onClick={()=>setSidebar(false)} alt="" />
-
-        <a onClick={()=>setSidebar(false)} href="#" className='sm:hover:border-b'>Home</a>
-        <a onClick={()=>setSidebar(false)} href="#service" className='sm:hover:border-b'>Skills</a>
-        <a onClick={()=>setSidebar(false)} href="#work" className='sm:hover:border-b'>Our Work</a>
-        <a onClick={()=>setSidebar(false)} href="#contact" className='sm:hover:border-b'>Contact Us</a>
+        <a
+          onClick={() => setSidebar(false)}
+          href="#"
+          className="sm:hover:border-b"
+        >
+          Home
+        </a>
+        <a
+          onClick={() => setSidebar(false)}
+          href="#service"
+          className="sm:hover:border-b"
+        >
+          Skills
+        </a>
+        <a
+          onClick={() => setSidebar(false)}
+          href="#work"
+          className="sm:hover:border-b"
+        >
+          Our Work
+        </a>
+        <a
+          onClick={() => setSidebar(false)}
+          href="#contact"
+          className="sm:hover:border-b"
+        >
+          Contact Us
+        </a>
       </div>
 
-      <div className='flex items-center gap-2 sm:gap-6'>
+      <div className="flex items-center gap-2 sm:gap-6">
+        <ToggleBtn theme={theme} setTheme={setTheme} />
 
-        <ToggleBtn theme={theme} setTheme={setTheme}/>
+        <img
+          src={theme === "dark" ? assets.menu_icon_dark : assets.menu_icon}
+          alt=""
+          className="w-8 sm:hidden"
+          onClick={() => setSidebar(true)}
+        />
 
-        <img src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon} alt="" 
-        className='w-8 sm:hidden' onClick={()=>setSidebar(true)}/>
-
-        <a href="#contact" className='text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white
-        px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all'>
+        <a
+          href="#contact"
+          className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white
+        px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all"
+        >
           Connect <img src={assets.arrow_icon} width={14} alt="" />
         </a>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
